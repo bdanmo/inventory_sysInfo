650bee177b8d3a9b3d36834865cf2a1a55e8e001
Brendan Moran <b.daniel.moran@gmail.com>
2022-02-08 17:48:38 -0500

    reformatted results buffer

--- inventory_sysInfo.py
+++ inventory_sysInfo.py
@@ -26,14 +26,14 @@ SerialNumber = d1.replace("\r", "").strip().split('\n')[-1]
 Model = d2.replace("\r", "").strip().split('\n')[1].strip()
 Manufacturer = d5.replace("\r", "").strip().split(':')[-1].strip()
 Hostname = d2.replace("\r", "").strip().split('\n')[3]
-TotalPhysicalMemory = d3.replace("\r", "").strip().split('\n')[-1]
+Memory = d3.replace("\r", "").strip().split('\n')[-1]
 Size = d4.replace("\r", "").strip().split('\n')[-1]
 
 buffer = [
 	F"SerialNumber:        {SerialNumber}",
-	F"Make/Model:          {Manufacturer} {Model}",
-	F"TotalPhysicalMemory: {format_size(int(TotalPhysicalMemory), 'RAM')}",
-	F"Size:                {format_size(int(Size), 'HD')}",
+	F"Description:         {Manufacturer} {Model}",
+	F"Memory:              {format_size(int(Memory), 'RAM')}",
+	F"DiskSize:            {format_size(int(Size), 'HD')}",
 	F"Hostname:            {Hostname}",
 	F"User:                {os.getlogin()}"
 ]
